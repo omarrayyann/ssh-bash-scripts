89fe64e2c66891b23cc99c972f8fe7c9186f8fd8
Omar <olr7742@nyu.edu>
2024-06-24 05:05:45 +0300

    update

--- double_port.sh
+++ double_port.sh
@@ -5,5 +5,14 @@ sed -i '' '26d;25d;24d' /Users/omarrayyann/.ssh/known_hosts
 HOST1="olr7742@greene.hpc.nyu.edu"
 HOST2="gv010.hpc.nyu.edu"
 
-ssh -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST1" &
-ssh -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST2" &
+get_password() {
+    cat password.txt
+}
+
+PASSWORD=$(get_password)
+
+ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST1" &
+ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST2" &
+
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST1" &
+sshpass -p ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST2" &
