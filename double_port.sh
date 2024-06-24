fcf18a4c0963ac05111fb03cc317af6f946ac92b
Omar <olr7742@nyu.edu>
2024-06-24 05:02:26 +0300

    update

--- double_port.sh
+++ double_port.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Remove specific lines from known_hosts file
+sed -i '' '26d;25d;24d' /Users/omarrayyann/.ssh/known_hosts
+
+# Define variables for SSH hosts
 HOST1="olr7742@greene.hpc.nyu.edu"
 HOST2="gv010.hpc.nyu.edu"
 
+# Forward ports for HOST1
 ssh -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST1" &
+
+# Forward ports for HOST2
 ssh -L 9876:localhost:9876 -L 9877:localhost:9877 "$HOST2" &
